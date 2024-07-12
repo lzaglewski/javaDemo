20835e5ce5e14467a80a19a28077a9b05d984b4a
Łukasz Żaglewski <zaglewski@oskarwegner.pl>
2024-07-12 15:35:12 +0200

    Events

--- src/main/java/com/example/demo/domain/member/MemberService.java
+++ src/main/java/com/example/demo/domain/member/MemberService.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.member;
 
 import com.example.demo.domain.common.log.LogMessage;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
@@ -12,18 +13,23 @@ import java.util.List;
 public class MemberService {
     private final MemberRepositoryInterface memberRepositoryInterface;
     private final DirectChannel logChannel;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public MemberService(MemberRepositoryInterface memberRepositoryInterface, @Qualifier("logChannel") DirectChannel logChannel) {
+    public MemberService(MemberRepositoryInterface memberRepositoryInterface, @Qualifier("logChannel") DirectChannel logChannel, ApplicationEventPublisher eventPublisher) {
         this.memberRepositoryInterface = memberRepositoryInterface;
         this.logChannel = logChannel;
+        this.eventPublisher = eventPublisher;
     }
 
     public void createMember(String name, String email) {
         Member member = new Member(name, email);
+        memberRepositoryInterface.save(member);
 
         LogMessage logMessage = new LogMessage("Member created", System.currentTimeMillis());
         logChannel.send(MessageBuilder.withPayload(logMessage).build());
-        memberRepositoryInterface.save(member);
+        
+        MemberCreatedEvent event = new MemberCreatedEvent(this, name, email);
+        eventPublisher.publishEvent(event);
     }
 
     public List<Member> getMembersByEmail(String email) {
